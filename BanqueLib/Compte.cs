bc16588fc1de686027cd594826b04d14b68af293
RemiLabelle <6327999@cstjean.qc.ca>
2025-03-28 04:59:13 -0400

    Projet fini

---
+++ BanqueLib/Compte.cs
@@ -0,0 +1,193 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BanqueLib
+{
+    public enum StatutCompte { Ok, Gelé };
+
+    public class Compte
+    {
+        #region ----- champs -----
+
+        private readonly int _numCompte;
+        private string _detenteur;
+        private decimal _solde;
+        private bool _estGele;
+        private StatutCompte _statut;
+
+        #endregion
+        #region ----- constructeurs -----
+
+        public Compte(int numero, string detenteur, decimal solde = 0, StatutCompte statut = StatutCompte.Ok)
+        {
+            if (numero <= 0)
+                throw new ArgumentOutOfRangeException(message: $"Numéro ('{numero}') must be a non-negative and non-zero value.", paramName: "Numéro", actualValue: numero);
+            this._numCompte = numero;
+
+            SetDétenteur(detenteur);
+
+            if ((((solde % 1) * 100) % 1) != 0)
+                throw new ArgumentOutOfRangeException(message: $"Solde ('{solde}') must be equal to '0.00'.", paramName: "Solde", actualValue: solde);
+            if (solde < 0)
+                throw new ArgumentOutOfRangeException(message: $"Solde ('{solde}') must a non-negative value.", paramName: "Solde", actualValue: solde);
+            this._solde = solde;
+
+            this._statut = statut;
+
+            this._estGele
+                = this._statut == StatutCompte.Ok ? false
+                : true;
+        }
+
+        #endregion
+        #region ----- getters -----
+
+        public int Numéro => _numCompte;
+        public string Détenteur => _detenteur;
+        public decimal Solde => _solde;
+        public bool EstGelé => _estGele;
+        public StatutCompte Statut => _statut;
+
+        #endregion
+        #region ----- setters -----
+
+        [MemberNotNull("_detenteur")]
+        public void SetDétenteur(string detenteur)
+        {
+            if (detenteur == null)
+                throw new ArgumentNullException(paramName: "Détenteur");
+            if (detenteur.Trim() == "")
+                throw new ArgumentException(message: "The value cannot be an empty string or composed entirely of whitespace.", paramName: "Détenteur");
+            this._detenteur = detenteur.Trim();
+        }
+
+        #endregion
+        #region ----- méthodes calculantes -----
+
+        public string Description()
+        {
+            string output = "";
+
+            for (int i = 0; i < 8; i++) //rangées
+            {
+                output += "[RL] ";
+
+                for (int j = 0; j < 55; j++)  //colonnes
+                {
+                    if (i == 0 || i == 7)
+                    {
+                        output += "*";
+                    }
+                    else if (i == 2 && j == 5)
+                    {
+                        string txt = $"COMPTE : {Numéro}";
+                        output += txt;
+                        j += (txt.Length - 1);
+                    }
+                    else if (i == 3 && j == 9)
+                    {
+                        string txt = $"De : {Détenteur}";
+                        output += txt;
+                        j += (txt.Length - 1);
+                    }
+                    else if (i == 4 && j == 6)
+                    {
+                        string txt = $"Solde : {Solde:C2}";
+                        output += txt;
+                        j += (txt.Length - 1);
+                    }
+                    else if (i == 5 && j == 5)
+                    {
+                        string txt = $"Statut : {Statut}";
+                        output += txt;
+                        j += (txt.Length - 1);
+                    }
+                    else
+                    {
+                        if (j == 0 || j == 54)
+                        {
+                            output += "*";
+                        }
+                        else
+                        {
+                            output += " ";
+                        }
+                    }
+                }
+
+                output += "\n";
+            }
+
+            return output;
+        }
+        public bool PeutDéposer(decimal montant = 1)
+        {
+            if ((((montant % 1) * 100) % 1) != 0)
+                throw new ArgumentOutOfRangeException(message: $"montant ('{montant}') must be equal to '1.00'.", paramName: "montant", actualValue: montant);
+            if (montant <= 0)
+                throw new ArgumentOutOfRangeException(message: $"montant ('{montant}') must be a non-negative and non-zero value.", paramName: "montant", actualValue: montant);
+
+            return
+                !EstGelé ? true : false;
+        }
+        public bool PeutRetirer(decimal? montant = null)
+        {
+            if (montant == null) montant = Solde;
+
+            if ((((montant % 1) * 100) % 1) != 0)
+                throw new ArgumentOutOfRangeException(message: $"montant ('{montant}') must be equal to '1.00'.", paramName: "montant", actualValue: montant);
+            if (montant <= 0)
+                throw new ArgumentOutOfRangeException(message: $"montant ('{montant}') must be a non-negative and non-zero value.", paramName: "montant", actualValue: montant);
+
+            if (montant <= Solde && !EstGelé) return true;
+            else return false;
+        }
+
+        #endregion
+        #region ----- méthodes modifiantes -----
+
+        public void Déposer(decimal montant)
+        {
+            if (PeutDéposer(montant) == false) //check comportement (si arg except dans PeutDéposer transpose)
+                throw new InvalidOperationException(message: "Déposer");
+
+            this._solde += montant;
+        }
+        public void Retirer(decimal montant)
+        {
+            if (PeutRetirer(montant) == false) //idem
+                throw new InvalidOperationException(message: "Retirer");
+
+            this._solde -= montant;
+        }
+        public decimal Vider()
+        {
+            if (Solde == 0 || EstGelé)
+                throw new InvalidOperationException(message: "Vider");
+
+            decimal contenuCompte = Solde;
+            this._solde = 0;
+            return contenuCompte;
+        }
+        public void Geler()
+        {
+            if (EstGelé)
+                throw new InvalidOperationException(message: "Geler");
+
+            this._estGele = true;
+            this._statut = StatutCompte.Gelé;
+        }
+        public void Dégeler() 
+        {
+            if (!EstGelé)
+                throw new InvalidOperationException(message: "Dégeler");
+
+            this._estGele = false;
+            this._statut = StatutCompte.Ok;
+        }
+
+        #endregion
+    }
+}
---
+++ ConsoleSimulerCompte/ConsoleSimulerCompte.cs
@@ -0,0 +1,146 @@
+﻿using BanqueLib;
+using System.Reflection.Metadata.Ecma335;
+
+Compte compte = new Compte(new Random().Next(100, 1000), "Rémi Labelle");
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine(compte.Description());
+    Console.WriteLine("""
+    
+     1 - Modifier Détenteur
+     2 - Peut Déposer
+     3 - Peut retirer
+     4 - Peut retirer (montant)
+     5 - Déposer (montant)
+     6 - Retirer (montant)
+     7 - Vider
+     8 - Geler
+     9 - Dégeler
+     q - Quitter
+     r - Reset
+
+     Votre choix, Rémi Labelle ?
+
+    """);
+
+    switch (Console.ReadKey(true).KeyChar)
+    {
+        case '1':
+            compte.SetDétenteur("Rémi Labelle " + new Random().Next(1, 100));
+            Console.WriteLine($"** Détenteur modifié pour : {compte.Détenteur}.");
+            break;
+        case '2':
+            try
+            {
+                Console.WriteLine("** Peut déposer ? " + (compte.PeutDéposer() ? "Oui." : "Non."));
+            }
+            catch (ArgumentOutOfRangeException) 
+            {
+                Console.WriteLine("** Peut déposer ? Non.");
+            }
+
+            break;
+        case '3':
+            try
+            {
+                Console.WriteLine("** Peut retirer ? " + (compte.PeutRetirer() ? "Oui." : "Non."));
+            }
+            catch (ArgumentOutOfRangeException) 
+            {
+                Console.WriteLine("** Peut retirer ? Non.");
+            }
+            
+            break;
+        case '4':
+
+            decimal _ = Math.Round(Convert.ToDecimal(new Random().Next(0, 100) + new Random().NextDouble()), 2);
+
+            try
+            {
+                Console.WriteLine($"** Peut retirer {_:C2}? " + (compte.PeutRetirer(_) ? "Oui." : "Non."));
+            }
+            catch (ArgumentOutOfRangeException) 
+            {
+                Console.WriteLine($"** Peut retirer {_:C2} ? Non.");
+            }
+            
+            break;
+        case '5':
+
+            _ = Math.Round(Convert.ToDecimal(new Random().Next(0, 100) + new Random().NextDouble()), 2);
+
+            try
+            {
+                compte.Déposer(_);
+                Console.WriteLine($"** Dépôt de {_:C2}.");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"** Impossible de déposer {_:C2}.");
+            }
+            
+            break;
+        case '6':
+
+            _ = Math.Round(Convert.ToDecimal(new Random().Next(0, 100) + new Random().NextDouble()), 2);
+
+            try
+            {
+                compte.Retirer(_);
+                Console.WriteLine($"** Retrait de {_:C2}.");
+            }
+            catch (InvalidOperationException) 
+            {
+                Console.WriteLine($"** Impossible de retirer {_:C2}.");
+            }
+            
+            break;
+        case '7':
+            try
+            {
+                Console.WriteLine($"** Retrait complet de {compte.Vider():C2}.");
+            }
+            catch (InvalidOperationException) 
+            {
+                Console.WriteLine("** Impossible de vider un compte vide ou gelé.");
+            }
+
+            break;
+        case '8':
+            if (compte.EstGelé)
+            {
+                Console.WriteLine("** Impossible de geler un compte déjà gelé.");
+            }
+            else 
+            {
+                compte.Geler();
+                Console.WriteLine("** Le compte a été gelé.");
+            }
+            break;
+        case '9':
+            if (!compte.EstGelé)
+            {
+                Console.WriteLine("** Impossible de dégeler un compte non gelé.");
+            }
+            else
+            {
+                compte.Dégeler();
+                Console.WriteLine("** Le compte a été dégelé.");
+            }
+            break;
+        case 'q':
+            Environment.Exit(0); 
+            break;
+        case 'r':
+            compte = new Compte(new Random().Next(100, 1000), "Rémi Labelle");
+            Console.WriteLine("Un nouveau compte a été créé.");
+            break;
+        default:
+            Console.WriteLine(" Mauvais choix."); 
+            break;
+    }
+    Console.WriteLine("\n Appuyer sur ENTER pour continuer...");
+    Console.ReadLine();
+}
