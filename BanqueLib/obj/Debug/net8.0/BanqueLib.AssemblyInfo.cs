57c04b982c05f388cba3362188cb7adf0098df67
RemiLabelle <6327999@cstjean.qc.ca>
2025-04-25 10:31:41 -0400

    Ajout Winforms
    Ajouté un projet Winforms

--- BanqueLib/obj/Debug/net8.0/BanqueLib.AssemblyInfo.cs
+++ BanqueLib/obj/Debug/net8.0/BanqueLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BanqueLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc16588fc1de686027cd594826b04d14b68af293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfff62bf8847e3c1632fff7b5ca4d4cdc99602f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BanqueLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BanqueLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
